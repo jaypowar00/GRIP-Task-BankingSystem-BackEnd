22b9f012339178f623e00435b4283e425c33412c
RedRanger00 <jaypowar00@gmail.com>
2021-08-21 22:20:19 +0530

    [Update] added catch for mongoose.connect

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 let PORT = process.env.PORT || 5000;
 let uri = process.env.DB_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).catch(err=>console.log('Error!\n'+err));
 
 app.use(cors());
 app.use(express.json());
